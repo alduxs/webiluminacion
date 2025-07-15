f5e832b610c353796216e53e33d81f0fd13e8e80
Aldo Iñiguez <ainiguez@cajaprevision.org>
2025-07-15 19:31:15 -0300

    SCSS

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Web Iluminación</title>
+
+    <meta name="title" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="Aldo Iñiguez" />
+    <meta name="revisit-after" content="15 days" />
+    <meta name="robots" content="index follow" />
+
+    <!-- BOOTSTRAP CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/styles.css" />
 </head>
+
 <body>
-    
+
+    <header>
+
+        
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+    <!-- BOOTSTRAP JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
